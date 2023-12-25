7edf106be03026302653cc50e5859501e0c5968b
lethanhsonvsp <134608977+lethanhsonvsp@users.noreply.github.com>
2023-12-25 23:21:17 +0700

    Create nav.ino

---
+++ nav.ino
@@ -0,0 +1,107 @@
+//AGV Machine - Vinay Lanka
+
+//Import Motor - Cytron SPG30E-30K
+#include "Motor.h"
+#include <ros.h>
+#include <geometry_msgs/Twist.h>
+#include<PID_v1.h>
+#include <geometry_msgs/Vector3Stamped.h>
+#include <ros/time.h>
+
+
+ros::NodeHandle  nh;
+
+#define LOOPTIME 10
+
+Motor right(11,10,20,21);
+Motor left(9,8,18,19);
+
+volatile long encoder0Pos = 0;    // encoder 1
+volatile long encoder1Pos = 0;    // encoder 2
+
+double left_kp = 3.8 , left_ki = 0 , left_kd = 0.0;             // modify for optimal performance
+double right_kp = 4 , right_ki = 0 , right_kd = 0.0;
+
+double right_input = 0, right_output = 0, right_setpoint = 0;
+PID rightPID(&right_input, &right_output, &right_setpoint, right_kp, right_ki, right_kd, DIRECT);  
+
+double left_input = 0, left_output = 0, left_setpoint = 0;
+PID leftPID(&left_input, &left_output, &left_setpoint, left_kp, left_ki, left_kd, DIRECT);  
+
+float demandx=0;
+float demandz=0;
+
+double demand_speed_left;
+double demand_speed_right;
+
+unsigned long currentMillis;
+unsigned long prevMillis;
+
+float encoder0Diff;
+float encoder1Diff;
+
+float encoder0Error;
+float encoder1Error;
+
+float encoder0Prev;
+float encoder1Prev;
+
+void cmd_vel_cb( const geometry_msgs::Twist& twist){
+  demandx = twist.linear.x;
+  demandz = twist.angular.z;
+}
+
+ros::Subscriber<geometry_msgs::Twist> sub("cmd_vel", cmd_vel_cb );
+geometry_msgs::Vector3Stamped speed_msg;                                //create a "speed_msg" ROS message
+ros::Publisher speed_pub("speed", &speed_msg);                          //create a publisher to ROS topic "speed" using the "speed_msg" type
+
+double speed_act_left = 0;                    //Actual speed for left wheel in m/s
+double speed_act_right = 0;                    //Command speed for left wheel in m/s 
+
+void setup() {
+  nh.initNode();
+  nh.subscribe(sub);
+  nh.advertise(speed_pub);                  //prepare to publish speed in ROS topic
+//  Serial.begin(115200);
+  
+  rightPID.SetMode(AUTOMATIC);
+  rightPID.SetSampleTime(1);
+  rightPID.SetOutputLimits(-100, 100);
+
+  leftPID.SetMode(AUTOMATIC);
+  leftPID.SetSampleTime(1);
+  leftPID.SetOutputLimits(-100, 100);
+}
+
+void loop() {
+  currentMillis = millis();
+  if (currentMillis - prevMillis >= LOOPTIME){
+    prevMillis = currentMillis;
+
+    demand_speed_left = demandx - (demandz*0.1075);
+    demand_speed_right = demandx + (demandz*0.1075);
+  
+    left_setpoint = demand_speed_left*39.65;  //Setting required speed as a mul/frac of 1 m/s
+    right_setpoint = demand_speed_right*39.65;
+  
+    left_input = encoder0Diff;  //Input to PID controller is the current difference
+    right_input = encoder1Diff;
+    
+    leftPID.Compute();
+    left.rotate(left_output);
+    rightPID.Compute();
+    right.rotate(right_output);
+  }
+  publishSpeed(LOOPTIME);
+  nh.spinOnce();
+}
+
+//Publish function for odometry, uses a vector type message to send the data (message type is not meant for that but that's easier than creating a specific message type)
+void publishSpeed(double time) {
+  speed_msg.header.stamp = nh.now();      //timestamp for odometry data
+  speed_msg.vector.x = speed_act_left;    //left wheel speed (in m/s)
+  speed_msg.vector.y = speed_act_right;   //right wheel speed (in m/s)
+  speed_msg.vector.z = time/1000;         //looptime, should be the same as specified in LOOPTIME (in s)
+  speed_pub.publish(&speed_msg);
+//  nh.loginfo("Publishing odometry");
+}
